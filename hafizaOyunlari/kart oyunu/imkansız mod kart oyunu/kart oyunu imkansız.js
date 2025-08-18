816f8f81444424ad186cfb8db668e1f655f6d71b
CubukKrakerx <musluailesi102@gmail.com>
2025-08-18 19:58:16 +0300

    çalışmalarım klasörü root yapıldı

---
+++ hafizaOyunlari/kart oyunu/imkansız mod kart oyunu/kart oyunu imkansız.js
@@ -0,0 +1,69 @@
+const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+let matchedCards = 0; // Eşleşen kart sayısı
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.add('flipped');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMatch();
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.card === secondCard.dataset.card;
+
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.classList.add('matched'); // Eşleşen kartlara "matched" sınıfı ekle
+    secondCard.classList.add('matched'); // Eşleşen kartlara "matched" sınıfı ekle
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    matchedCards += 2; // Eşleşen kartları say
+
+    if (matchedCards === cards.length) {
+        document.getElementById('restartButton').style.display = 'block'; // Yeniden başla butonunu göster
+    }
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flipped');
+        secondCard.classList.remove('flipped');
+        resetBoard();
+    }, 1000);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 100);
+        card.style.order = randomPos;
+    });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+document.getElementById('restartButton').addEventListener('click', () => {
+    location.reload();
+});
